e94639759e679a81aeb78f1efd11f8552170c33f
Paolococco94 <case-logic@hotmail.it>
2018-04-13 22:33:02 +0200

    src files

---
+++ src/print.cpp
@@ -0,0 +1,106 @@
+/**
+
+**********************************
+*********** TERZO NODO ***********
+**********************************
+
+*/
+
+#include <signal.h>
+#include <ros/console.h>
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include <sstream>
+#include <string>
+#include <string.h>
+
+std::string comando; // global variable where save the command coming from the node2 "select.cpp"
+std::string dato;    // global variable where save the data coming from the node1 "publisher.cpp"
+
+std::string aa = "A"; // value that identifies the whole string  
+std::string nn = "N"; // value that identifies the name
+std::string ee = "E"; // value that identifies the age
+std::string cc = "C"; // value that identifies the graduation  
+std::string zz = "Z"; // value that identifies exit of program
+
+// Topic messages callback	
+void chatterCallback(const std_msgs::String::ConstPtr& msg){  
+	dato = msg->data;
+	
+}
+
+
+void chatterCallback2(const std_msgs::String::ConstPtr& msg2){  
+	comando = msg2->data; 
+        
+}
+
+
+// string printing function 
+void check(std::string dato,std::string comando){
+ std::string name;
+ std::string age;
+ std::string course;
+ 
+		
+	if (comando.compare(aa)==0){	
+		ROS_INFO("string: %s\n", dato.c_str());  
+	}	
+	
+	else if(comando.compare(nn)==0){
+	 	name = dato.substr(0, 11);
+		ROS_INFO("name: %s\n", name.c_str()); 
+	}
+	
+
+	else if(comando.compare(ee)==0){
+		age = dato.substr(12,2);
+		ROS_INFO("age: %s\n", age.c_str());
+	}
+	
+
+	else if  (comando.compare(cc)==0){
+		course = dato.substr(15);
+		ROS_INFO("course: %s\n", course.c_str());	 	
+	}
+	
+        
+	else if  (comando.compare(zz)==0){
+		ros::shutdown();
+	}
+
+	else
+		ROS_INFO("comando selezionato non valido\n");
+		
+	
+}
+
+
+int main(int argc, char **argv){
+// initialization
+
+ros::init(argc, argv, "print");
+ros::NodeHandle n;
+
+while(ros::ok()){
+// listener for the data
+   ros::Subscriber sub = n.subscribe("chatter", 1000, chatterCallback);
+	while(dato == ""){ 
+	ros::spinOnce();
+	}
+// listener for the comand  
+  ros::Subscriber sub2 = n.subscribe("chatter2", 1000, chatterCallback2);
+  	while(comando ==""){
+ 	ros::spinOnce();
+	}
+
+	check(dato,comando);	
+	comando = ""; //la stringa comando deve essere vuota dopo la funzione check
+		      //altrimenti il cliclo [while(comando=="")] non viene eseguito
+		      //dopo il lancio del programma 
+
+}
+
+return 0;
+}
+
---
+++ src/select.cpp
@@ -0,0 +1,70 @@
+/**
+
+**********************************
+********* SECONDO NODO ***********
+**********************************
+
+*/
+
+
+
+
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include <iostream>       // std::cout
+#include <string>         // std::string
+char scelta;
+
+void stampaMenu (){
+  std::cout << "\n";
+  std::cout << "****************************************************************\n";
+  std::cout << "Menù: \n";
+  std::cout << "Seleziona operazione da eseguire\n" ;
+  std::cout << "Digita 'A' per stampare tutto il messaggio\n" ;
+  std::cout << "Digita 'N' per stampare il nome\n" ;
+  std::cout << "Digita 'E' per stampare l'età\n" ;
+  std::cout << "Digita 'C' per stampare il corso di laurea\n";
+  std::cout << "Digita 'Z' per uscire dal programma\n";
+  std::cout << "\n";
+  std::cout << "****************************************************************\n";
+}
+
+
+int main(int argc, char **argv){
+// initialization
+	ros::init(argc, argv, "select");
+	ros::NodeHandle n;
+// publisher for selecting command
+	ros::Publisher chatter_pub = n.advertise<std_msgs::String>("chatter2", 1000);	
+
+  	
+	while (ros::ok()){
+	stampaMenu();
+		std::cin >> scelta;
+		ROS_INFO("hai selezionato %c",scelta);
+		std_msgs::String msg2;
+		std::stringstream ss2;
+		ss2 << scelta;
+		msg2.data = ss2.str();
+		chatter_pub.publish(msg2);           
+		ros::Rate loop_rate(1);
+		
+		ros::spinOnce();// processing of incoming messages
+        			// When a message arrives, ROS pushes your subscriber callback onto a queue.
+        			// It does not call it immediately. ROS only processes your callbacks when
+        			// you tell it to with ros::spinOnce()
+		
+		loop_rate.sleep();
+		
+		
+		if(scelta=='Z'){
+		ros::shutdown();
+		}		
+  }
+
+
+return 0;
+
+}
+
+
